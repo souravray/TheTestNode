76aaec6e086611496287787eaafea7fa23692973
Sourav Ray <me@raysourav.com>
2012-05-22 23:36:20 +0530

    indent main.js

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
 var http = require('http'); 
 var proxyName = require('./proxy');
 http.createServer(function (req, resp) { 
-				console.log("\n----Request header starts----");
-        for (e in req.headers) { 
-                console.log(e+": "+req.headers[e]); 
-        } 
-				console.log("----Request header ends----\n");
-				var proxy = http.createClient(80, proxyName)
-					, proxyRequest = proxy.request(req.method, req.url, req.headers);
+	console.log("\n----Request header starts----");
+	for (e in req.headers) { 
+		console.log(e+": "+req.headers[e]); 
+	} 
+	console.log("----Request header ends----\n");
 
-				proxyRequest.addListener('response', function (proxyResponse) {
-					console.log("----Response header starts----");
-					for (e in proxyResponse.headers) { 
-                console.log(e+": "+proxyResponse.headers[e]); 
-        	} 
-					console.log("----Response header ends----\n");
-					proxyResponse.addListener('data', function(chunk) {
-						resp.write(chunk, 'binary');
-					});
-					proxyResponse.addListener('end', function() {
-						resp.end();
-					});
-					resp.writeHead(proxyResponse.statusCode, proxyResponse.headers);
-				});
-				req.addListener('data', function(chunk) {
-					proxyRequest.write(chunk, 'binary');
-				});
-				req.addListener('end', function() {
-					proxyRequest.end();
-				});
-        
+	var proxy = http.createClient(80, proxyName)
+	, proxyRequest = proxy.request(req.method, req.url, req.headers);
+	
+	proxyRequest.addListener('response', function (proxyResponse) {
+		console.log("----Response header starts----");
+		for (e in proxyResponse.headers) { 
+			console.log(e+": "+proxyResponse.headers[e]); 
+		} 
+		console.log("----Response header ends----\n");
+
+		proxyResponse.addListener('data', function(chunk) {
+			resp.write(chunk, 'binary');
+		});
+
+		proxyResponse.addListener('end', function() {
+			resp.end();
+		});
+		resp.writeHead(proxyResponse.statusCode, proxyResponse.headers);
+	});
+	
+	req.addListener('data', function(chunk) {
+		proxyRequest.write(chunk, 'binary');
+	});
+	
+	req.addListener('end', function() {
+		proxyRequest.end();
+	});        
 }).listen(7979, function(){
   console.log("Test-Node node is running on port: 7979");
 }); 
